d1a9386909393862b1a7894406a5018d21cf04a3
Jonathan Deltoro <jonathandeltoro@Jonathans-MacBook-Air.local>
2020-09-11 21:24:46 -0600

    added both tests, created hook

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,19 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
 
-test("form shows success message on submit with form details", () => {});
+
+
+
+
+test("form header renders", () => {
+    const {getByText} = render (<CheckoutForm/>)
+    const header = getByText (/Checkout Form/i)
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    const { getByTestId } = render (<CheckoutForm/>)
+    const successMessage = findByTestId (/successMessage/I)
+    expect (successMessage).toBeTruthy;
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+
+
+
+export const useForm = (initialValue) => {
+ // inputs - handleChange, state (dynamically manage any number of inputs)
+    const [values, setValues] = useState (initialValue)
+
+    const handlechanges = e => {
+        setValues ({
+            ...values,
+            [e.target.name] : e.target.value
+        })
+    }
+
+
+return [values, handlechanges]
+
+}
